b1ce9665d4d4526efde2b325b2551c50400ec8bd
Matheus Rodrigues <matheussr61@gmail.com>
2021-04-21 18:40:30 +0000

    Fix run tests script

--- lib/runtests.sh
+++ lib/runtests.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-#for file in tests/*; do
-for i in {1..100}; do
-	echo "Running #$i"
-#	echo "Running $file"
-	LD_LIBRARY_PATH=../deps/lua-memory/src LUA_CPATH='../deps/lua-memory/src/?.so;;' lua tests/simple_send.lua
-#	echo "$file executed with no errors!"
-#done
+for file in tests/*; do
+	LD_LIBRARY_PATH=../deps/lua-memory/src:$LD_LIBRARY_PATH LUA_CPATH='../deps/lua-memory/src/?.so;;' lua $file > /dev/null
+	echo "File $file executed with no errors"
 done
